65ac564268a2c3dee3b47b9f206588ea4e7f315a
Radovan Klembara <radovan@klembara.pro>
2021-02-08 19:07:22 +0100

    git ignore

---
+++ SurveyAPP-FISEC/SurveyAPP-FISEC/SurveyAPP_FISECApp.swift
@@ -0,0 +1,20 @@
+//
+//  SurveyAPP_FISECApp.swift
+//  SurveyAPP-FISEC
+//
+//  Created by Radovan Klembara on 08/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct SurveyAPP_FISECApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
